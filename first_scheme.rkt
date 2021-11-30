09ed7694fd87b60119087d16934d9db3a0f5fd99
sai vishnu anudeep kadiyala <54953858+anudeep-17@users.noreply.github.com>
2021-11-30 17:55:19 -0500

    assignment part1 update

--- first_scheme.rkt
+++ first_scheme.rkt
@@ -1,6 +1,20 @@
 #lang scheme
-(print 2)
-(define x 10)
-(define a 5)
-(+ a x)
+( define (list_absval lst)
+   (
+   
+    let loop((lst lst))
+     (
+      when (pair? lst)
+       
+       (define x (car lst))
+       (display (abs x))
+       (display " ")
+       (loop (cdr lst))
+      )
+    )
+(newline)
+)
+
+(define lst (list -1 -2 -2 -10 -11 -1111 -324314.12312))
+(list_absval lst)
 
